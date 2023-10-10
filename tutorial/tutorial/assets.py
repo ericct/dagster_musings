61f1e2f45c27d52fafb15810baa2d040b5b553d8
Eric Cheminot <eric.cheminot@gmail.com>
2023-10-10 14:42:51 +0000

    File IO Manager

--- tutorial/tutorial/assets.py
+++ tutorial/tutorial/assets.py
@@ -1,6 +1,6 @@
 import json
 import os
-import pandas as pd  # Add new imports to the top of `assets.py`
+import pandas as pd 
 import requests
 from dagster import (
     AssetExecutionContext,
@@ -11,24 +11,27 @@ from dagster import (
 import base64
 from io import BytesIO
 import matplotlib.pyplot as plt
+from typing import Dict, List  # add imports to the top of `assets.py`
 
 
-@asset # add the asset decorator to tell Dagster this is an asset
-def topstory_ids() -> None:
+@asset
+def topstory_ids() -> List:  # modify return type signature
     newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
     top_new_story_ids = requests.get(newstories_url).json()[:100]
 
-    os.makedirs("data", exist_ok=True)
-    with open("data/topstory_ids.json", "w") as f:
-        json.dump(top_new_story_ids, f)
+    return (
+        top_new_story_ids  # return top_new_story_ids and the I/O manager will save it
+    )
 
 
-@asset(deps=[topstory_ids])
-def topstories(context: AssetExecutionContext) -> None:
+@asset  # remove deps parameter
+def topstories(
+    context: AssetExecutionContext,
+    topstory_ids: List,  # add topstory_ids as a function argument
+) -> pd.DataFrame:  # modify the return type signature
     logger = get_dagster_logger()
 
-    with open("data/topstory_ids.json", "r") as f:
-        topstory_ids = json.load(f)
+    # remove manually loading topstory_ids
 
     results = []
     for item_id in topstory_ids:
@@ -41,24 +44,28 @@ def topstories(context: AssetExecutionContext) -> None:
             logger.info(f"Got {len(results)} items so far.")
 
     df = pd.DataFrame(results)
-    df.to_csv("data/topstories.csv")
+
+    # remove manually saving df
 
     context.add_output_metadata(
         metadata={
-            "num_records": len(df),  # Metadata can be any key-value pair
+            "num_records": len(df),
             "preview": MetadataValue.md(df.head().to_markdown()),
-            # The `MetadataValue` class has useful static methods to build Metadata
         }
     )
 
+    return df  # return df and the I/O manager will save it
 
-@asset(deps=[topstories])
-def most_frequent_words(context: AssetExecutionContext) -> None:
+
+@asset  # remove deps parameter
+def most_frequent_words(
+    context: AssetExecutionContext,
+    topstories: pd.DataFrame,  # add topstories as a function argument
+) -> Dict:  # modify the return type signature
     stopwords = ["a", "the", "an", "of", "to", "in", "for", "and", "with", "on", "is"]
 
-    topstories = pd.read_csv("data/topstories.csv")
+    # remove manually loading topstory_ids
 
-    # loop through the titles and count the frequency of each word
     word_counts = {}
     for raw_title in topstories["title"]:
         title = raw_title.lower()
@@ -67,29 +74,25 @@ def most_frequent_words(context: AssetExecutionContext) -> None:
             if cleaned_word not in stopwords and len(cleaned_word) > 0:
                 word_counts[cleaned_word] = word_counts.get(cleaned_word, 0) + 1
 
-    # Get the top 25 most frequent words
     top_words = {
         pair[0]: pair[1]
         for pair in sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:25]
     }
 
-    # Make a bar chart of the top 25 words
     plt.figure(figsize=(10, 6))
     plt.bar(list(top_words.keys()), list(top_words.values()))
     plt.xticks(rotation=45, ha="right")
     plt.title("Top 25 Words in Hacker News Titles")
     plt.tight_layout()
 
-    # Convert the image to a saveable format
     buffer = BytesIO()
     plt.savefig(buffer, format="png")
     image_data = base64.b64encode(buffer.getvalue())
 
-    # Convert the image to Markdown to preview it within Dagster
     md_content = f"![img](data:image/png;base64,{image_data.decode()})"
 
-    with open("data/most_frequent_words.json", "w") as f:
-        json.dump(top_words, f)
+    # remove manually saving top_words
 
-    # Attach the Markdown content as metadata to the asset
     context.add_output_metadata(metadata={"plot": MetadataValue.md(md_content)})
+
+    return top_words  # return top_words and the I/O manager will save it
